7d47922b342fb43bee234a85210389e554e77b77
SpykeTiger <josevitorrequena@gmail.com>
2024-06-05 16:07:05 -0300

    Fixed flake.nix

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,41 @@
+{self, ...}: 
+
 {
-  # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
+	imports = [
+		./imports.nix
+	];
+
+	colorschemes.gruvbox.enable = true;
+
+	plugins = {
+		lualine.enable = true;
+		luasnip.enable = true;
+
+		#All commands available
+		telescope.enable = true;
+		# $ nvim . will open oil instead of netrw
+		oil.enable = true;
+		# all parsers included, simple as that
+		treesitter.enable = true;
+
+		lsp = {
+			enable = true;
+			servers = {
+				tsserver.enable = true;
+				lua-ls.enable = true;
+			};
+		};
+
+		nvim-cmp = {
+			enable = true;
+			autoEnableSources = true;
+			sources = [
+			{name = "nvim_lsp";}
+			{name = "path";}
+			{name = "buffer";}
+			];
+
+		
+		};
+	};
 }
